7f6a7ae77e115a78c67640c70d12388b2547dccb
= <=>
2023-09-07 18:56:59 +0100

    add sortBy to category_list

--- task/views.py
+++ task/views.py
@@ -4,7 +4,13 @@ from django.shortcuts import render
 from .models import Category
 
 def category_list(request):
-    categories = Category.objects.all()
+    orderBy = request.GET.get('orderBy')
+    if orderBy == 'created' or orderBy == '-created':
+        categories = Category.objects.order_by(orderBy)
+    elif orderBy == 'title' or orderBy == '-title':
+        categories = Category.objects.order_by(orderBy)
+    else:
+        categories = Category.objects.all()
     return render(request, 'category_list.html', {'categories': categories})
     
     
